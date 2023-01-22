cdb08cec0629dc75203f17d7e096023c8315baa7
robinozer <robin.ozer@outlook.com>
2023-01-22 18:13:35 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+QUESTIONS = [
+    # This question bank can be moved into a separate file
+    # when the amount of questions increases.
+    {
+        'question': "Q1. Aureolin is a shade of what color?",
+        'options': "1. Yellow\n2. Green\n3. Red\n4. Orange",
+        'correct_option': '1',
+        'correct_answer': 'Yellow'
+    },
+    {
+        'question': "Q2. Cerulean is a shade of what color?",
+        'options': "1. Brown\n2. Blue\n3. Grey\n4. White",
+        'correct_option': '2',
+        'correct_answer': 'Blue'
+    },
+    {
+        'question': "Q3 Celadon is a shade of what color?",
+        'options': "1. White\n2. Black\n3. Grey\n4. Green",
+        'correct_option': '4',
+        'correct_answer': 'Green'
+    },
+    {
+        'question': "Q4 Maroon is a shade of what color?",
+        'options': "1. Pink\n2. Blue\n3. Red\n4. Yellow",
+        'correct_option': '3',
+        'correct_answer': 'Red'
+    },
+    {
+        'question': "Q5 Umber is a shade of what color?",
+        'options': "1. Yellow\n2. Red\n3. Brown\n4. Orange",
+        'correct_option': '3',
+        'correct_answer': 'Brown'
+    }
+]
